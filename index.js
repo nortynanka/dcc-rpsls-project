ad1adace170e413f5811fec5cd0195dafe4dd733
Kayl Guise <kaylee.guise@gmail.com>
2022-04-06 16:11:43 -0500

    index.js framework set up

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 "use strict"
+
+class baseGame {
+    constructor(){
+        this.playerOne = "";
+        this.playerTwo = "";
+    }
+
+displayWelcome();
+displayRules();
+determineNumberOfPlayers();
+runRounds();
+compareGestures();
+checkWinner();
+displayWinner();
+runGame();
+askToRestart();
+
+}
+
+class handGesture {
+    constructor(){
+        this.gestureName = "";
+    }
+}
+
+class playerInfo {
+    constructor(){
+        this.playerName = "";
+        this.score = "";
+        this.roundChoice = "";
+        this.gestures = "";
+    }
+
+displayName();
+setName();
+chooseGesture();
+
+}
+
+class playerInfo extends humanPlayer {
+    constructor(){
+
+    }
+
+chooseGesture();
+
+}
+
+class playerInfo extends computerPlayer {
+    constructor(){
+
+    }
+
+chooseGesture();
+
+}
